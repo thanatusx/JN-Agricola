d3cf8dc34c7d6cb6e92dc6593848cad1d6a12e53
Clebto <mastergu657@gmail.com>
2024-01-07 02:07:45 -0300

    main

--- script/index.js
+++ script/index.js
@@ -1,30 +1,8 @@
 const menu = document.querySelector('.menu');
-const NavMenu = document.querySelector('.nav-menu');
+const navmenu = document.querySelector('.nav-menu');
 
 menu.addEventListener('click', () => {
     menu.classList.toggle('ativo');
-    NavMenu.classList.toggle('ativo');
+    navmenu.classList.toggle('ativo');
 })
 
-const dropdown = document.querySelector('.dropdown');
-const dropdownmenu = document.querySelector('.dropdown-menu');
-
-dropdownmenu.addEventListener('click', () => {
-    dropdown.classList.toggle('ativo');
-});
-
-
-var cont = 1
-document.getElementById('radio1').checked = true
-
-setInterval(() =>{
-    proximaImg()
-},3000)
-
-function proximaImg(){
-    cont++
-    if (cont > 5){
-        cont = 1
-    }
-    document.getElementById('radio'+cont).checked = true
-}
